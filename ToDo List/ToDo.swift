217cf44cf4b6be7516e942b68d630918397b69e8
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-23 15:09:23 -0800

    create toDo class and populate properties on table cell rows

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Mariano Echegoyen on 12/23/17.
+//  Copyright © 2017 Mariano Echegoyen. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
