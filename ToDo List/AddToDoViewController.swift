71164c959f63df0690513dcee30882d1889f7167
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-24 11:43:37 -0800

    unwrap optionals

--- ToDo List/AddToDoViewController.swift
+++ ToDo List/AddToDoViewController.swift
@@ -22,13 +22,16 @@ class AddToDoViewController: UIViewController {
     }
     @IBAction func addTapped(_ sender: Any) {
         let toDo = ToDo()
-        toDo.name = titleTextField.text!
-        toDo.important = importantSwitch.isOn
         
-        previousVC.toDos.append(toDo)
-        previousVC.tableView.reloadData()
-        
-        navigationController?.popViewController(animated: true)
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+            
+            previousVC.toDos.append(toDo)
+            previousVC.tableView.reloadData()
+            
+            navigationController?.popViewController(animated: true)
+        }
     }
     
 }
--- ToDo List/ToDoTableViewController.swift
+++ ToDo List/ToDoTableViewController.swift
@@ -52,8 +52,9 @@ class ToDoTableViewController: UITableViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let addVC = segue.destination as! AddToDoViewController
-        addVC.previousVC = self
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
     }
 
 }
