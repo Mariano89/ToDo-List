70430ec3246d1195324754ac0bc009267c5d6fa1
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-23 17:18:21 -0800

    create addToDo view controller and connect outlets / actions

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by Mariano Echegoyen on 12/23/17.
+//  Copyright © 2017 Mariano Echegoyen. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
